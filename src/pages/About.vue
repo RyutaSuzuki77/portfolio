d21b6ae0037578811c787cd4408a76bb3ef3c504
dragon <ryuta.bump@gmail.com>
2019-02-17 01:10:18 +0900

    aboutページ作成 career.vueのページタイトルの誤字修正

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
     <Pagetitle pagetitle="About"></Pagetitle>
+    <table>
+      <tbody>
+        <tr v-for="line in table">
+          <td v-for="(item) in line">
+            <p v-if="item.indexOf('<a') !== -1">
+              <a v-html="item"></a>
+            </p>
+            <p v-else>
+              {{item}}
+            </p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,6 +23,18 @@ import Pagetitle from '@/components/Pagetitle.vue'
 
 export default{
   name:'about',
+  data(){
+    return{
+      table:[
+        ['Name','Suzuki Ryuta'],
+        ['NickName','Dragon'],
+        ['Birthday','1994-09-25'],
+        ['License','Java SE8 Silver, 普通免許(AT)'],
+        ['Github','<a href="https://github.com/RyutaSuzuki77">RyutaSuzuki77</a>'],
+        ['Qiita','<a href="https://qiita.com/dragonnn">dragonnn</a>']
+      ]
+    }
+  },
   components:{
     Pagetitle
   }
